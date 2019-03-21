064b7cb33329e57122e835cd6f429d2c54a95e75
arohithr8 <46582843+arohithr8@users.noreply.github.com>
2019-03-21 15:04:17 -0500

    Update CalculatorExtensions.cs

--- Demo/CalculatorExtensions.cs
+++ Demo/CalculatorExtensions.cs
@@ -34,7 +34,23 @@
         }
         
         
+        
+        
+        
+        
         public static int Add2(this int a, int b)
+        {
+            return a + b;
+        }
+        public static int Add3(this int a, int b)
+        {
+            return a + b;
+        }
+        public static int Add4(this int a, int b)
+        {
+            return a + b;
+        }
+        public static int Add5(this int a, int b)
         {
             return a + b;
         }
